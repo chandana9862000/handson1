ebe409f8e805a8ea3a0577237267fea74402b393
unknown <chandana9862000@gmail.com>
2022-01-07 12:31:25 +0530

    sql

---
+++ handson 1 sql.sql
@@ -0,0 +1,60 @@
+create database handson
+create table salesman(
+salesman_ID numeric(5) primary key,
+Name varchar(30) not null,
+city varchar(15) not null,
+commission decimal(5,2) not null
+)
+
+select * from salesman
+insert into salesman values(5001,'James Hoog','Newyork',0.15)
+insert into salesman values(5002,'Nail Knite','Paris',0.13)
+insert into salesman values(5005,'Pit Alex','London',0.11)
+insert into salesman values(5006,'Mc Lyon','Paris',0.14)
+insert into salesman values(5007,'paul Adam','Rome',0.13)
+insert into salesman values(5003,'Lauson Hen','Sanjose',0.12)
+select name,commission from salesman
+select name,city from salesman where city = 'paris'
+
+drop table orders
+select * from orders
+insert into orders values(70001,150.5,'2012-05-10',3005,5002)
+insert into orders values(70009,270.65,'2012-09-10',3001,5005)
+insert into orders values(70002,65.26,'2012-05-10',3002,5001)
+insert into orders values(70004,110.5,'2012-08-17',3009,5003)
+insert into orders values(70007,948.5,'2012-09-10',3005,5002)
+insert into orders values(70005,2400.6,'2012-07-27',3005,5001)
+insert into orders values(70008,5760,'2012-09-10',3002,5001)
+insert into orders values(70010,1983.43,'2012-10-10',3004,5006)
+insert into orders values(70003,2480.4,'2012-10-10',3009,5003)
+insert into orders values(70012,250.45,'2012-06-27',3008,5002)
+insert into orders values(70011,75.29,'2012-08-17',3003,5007)
+insert into orders values(70013,3045.6,'2012-04-25',3002,5001)
+select ord_date,salesman_id,ord_no,purch_amt from orders
+
+
+create table customer(
+customer_id numeric(5) primary Key,
+cust_Name varchar(30),
+city varchar(15),
+grade numeric(3),
+salesman_id numeric(5)
+)
+
+
+select customer_id,cust_name,city,grade,salesman_id from customer where grade=200
+select ord_no,ord_date,purch_amt from orders where salesman_id = 5001
+
+select avg(purch_amt) as 'avg' from orders
+select count(*) as 'count' from salesman
+select count(*) as 'count' from customer
+select * from customer
+select max(purch_amt) as 'maxamt' from orders
+select city,max(grade) as 'max' from customer group by city 
+select *from customer
+select max(purch_amt) as 'max purch',customer_id from orders group by customer_id
+
+
+
+
+
